99d76996c7f016f5a2b72740196ecae519bc4d2b
Deepkumar Patel <dspguru97@gmail.com>
2020-11-15 17:02:08 -0500

    readMeUpdate

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Patel_Deepkumar_FavouriteThings
+# My Favourite Things
+
+### About
+
+This project will let you know about my three favourite things and it is class project with the aim to learn how to fetch data from databse and how to display those data on front-end side. There is some interations with the displayed images which will give more information about particular thing.
+
+## Roadmap
+
+Here is the roadmap which will give you insights about how I plan and manage to do this work.
+
+[My Favourite Things Roadmap](https://https://docs.google.com/document/d/149aKOCXO_Xqa3CYuZO5dmQE46S6XSHEg304Gv2pDmkw/edit)
+
+### Languages Used To Create Web Page:
+
+1. javascript 
+2. Html
+3. Css
+4. AJAX
+5. JSON
+
+### Web Development Framework:
+
+1. sass 
+
+### Backend Development :
+
+1. Php
+2. MySQL
+3. PhpMyAdmin
+
+### Typography - Web Development
+
+1. Arial
+
+### Images
+
+- images are taken from "google".
+
+
+### Authors
+
+1. Deepkumar Patel(0953510)
+
+### License
+
+This project is licensed under ***MIT***
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { fetchData, postData } from "./modules/TheDataMiner.js";
+
 (() => {
 
     const imgSelector = document.querySelectorAll('.user-section img'),
@@ -6,34 +8,77 @@
         blurBG =document.querySelector('.mainCon');
 
 
-    function changeImage(){
-        imagePop.src = `images/${this.dataset.image}.jpg`;
-        console.log("change Image");
-    }
 
-    function lightBox(modal){
-        if(modal == null) return;
-        modal.classList.add('activePop');
-        blurBG.classList.add('blurBG');
-    }
+        console.log('loaded');
+    
+        function popErrorBox(message) {
+            alert("Something has gone horribly, horribly wrong");
+        }
 
-    function closeBox(modal)
-      {
-        if(modal == null) return;
-        modal.classList.remove('activePop');
-        blurBG.classList.remove('blurBG');
-      }
+        function retrieveProjectInfo(event) {
+            // test for an ID
+            if(!event.target.id){ return }
+    
+            fetchData(`./includes/index.php?id=${event.target.id}`).then(data => console.log(data)).catch(err => console.log(err));
+        }
+    
+        function renderPortfolioThumbnails(thumbs)
+        {
+    
+            let userSection = document.querySelector('.user-section'),
+                userTemplate = document.querySelector('#user-template').content;
+    
+            for (let user in thumbs) {
+                let currentUser = userTemplate.cloneNode(true),
+                    currentUserText = currentUser.querySelector('.user').children;
+    
+                currentUserText[1].src = `images/${thumbs[user].image}`;
+                currentUserText[1].id = thumbs[user].id;
+    
+                // add this new user to the view
+                userSection.appendChild(currentUser);
+            }
+            
+            // Event Delegation 
+            userSection.addEventListener("click", retrieveProjectInfo);
+        }
 
-    imgSelector.forEach(img => img.addEventListener('click', function(){
-        const modal = document.querySelector(img.dataset.modal);
-        lightBox(modal);
-      }));
 
-      closeButton.forEach(button => button.addEventListener('click', function(){
-        const modal = button.closest('.lightbox');
-        closeBox(modal);
-      }));
+        fetchData("./includes/index.php").then(data => renderPortfolioThumbnails(data)).catch(err => { console.log(err); popErrorBox(err); });
 
-      imgSelector.forEach(anim => anim.addEventListener("click", changeImage));
+
+
+
+
+    // light Box Work
+    // function changeImage(){
+    //     imagePop.src = `images/${this.dataset.image}.jpg`;
+    //     console.log("change Image");
+    // }
+
+    // function lightBox(modal){
+    //     if(modal == null) return;
+    //     modal.classList.add('activePop');
+    //     blurBG.classList.add('blurBG');
+    // }
+
+    // function closeBox(modal)
+    //   {
+    //     if(modal == null) return;
+    //     modal.classList.remove('activePop');
+    //     blurBG.classList.remove('blurBG');
+    //   }
+
+    // imgSelector.forEach(img => img.addEventListener('click', function(){
+    //     const modal = document.querySelector(img.dataset.modal);
+    //     lightBox(modal);
+    //   }));
+
+    //   closeButton.forEach(button => button.addEventListener('click', function(){
+    //     const modal = button.closest('.lightbox');
+    //     closeBox(modal);
+    //   }));
+
+    //   imgSelector.forEach(anim => anim.addEventListener("click", changeImage));
 
 })();
